63e8f86d40b84dcbc3c96fc3dc57cef83d9dd774
jingyue00 <yuejing3963@gmail.com>
2018-06-23 21:36:49 -0400

    Merge Two Binary Tree

---
+++ src/MergeTwoBinaryTrees.java
@@ -0,0 +1,20 @@
+/**
+ * Solution: use recursion, it's important to clarify the input and return.
+ * Time Complexity: O(n)
+ * Space Complexity: O(n)
+ */
+public class MergeTwoBinaryTrees {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) {
+            return null;
+        } else if (t1 == null) {
+            return t2;
+        } else if (t2 == null) {
+            return t1;
+        }
+        TreeNode newNode = new TreeNode(t1.val + t2.val);
+        newNode.left = mergeTrees(t1.left, t2.left);
+        newNode.right = mergeTrees(t1.right, t2.right);
+        return newNode;
+    }
+}
