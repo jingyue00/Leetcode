23f1f92e927c0f8bd1f7b14b10924205212bfe83
jingyue00 <yuejing3963@gmail.com>
2018-06-29 23:23:56 -0400

    Same Tree

---
+++ src/SameTree.java
@@ -0,0 +1,13 @@
+/**
+ * Solution: each time check the value of two node.
+ * Time Complexity: O(n)
+ * Space Complexity: O(n)
+ */
+public class SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        if (p.val != q.val) return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
