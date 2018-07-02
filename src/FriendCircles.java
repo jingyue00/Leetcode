2e7e069352f8647bf75a76bf9f184235a53ff8f7
jingyue00 <yuejing3963@gmail.com>
2018-07-01 22:34:04 -0400

    Friend Circle

---
+++ src/FriendCircles.java
@@ -0,0 +1,27 @@
+/**
+ * Solution: set a visited array, each time if visited[i] == 0, call dfs to begin recursion.
+ * Time Complexity: O(n^2)
+ * Space Complexity: O(n)
+ */
+public class FriendCircles {
+    public int findCircleNum(int[][] M) {
+        int[] visited = new int[M.length];
+        int count = 0;
+        for (int i = 0; i < M.length; i++) {
+            if (visited[i] == 0) {
+                dfs(M, visited, i);
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public void dfs(int[][] M, int[] visited, int i) {
+        for (int j = 0; j < M.length; j++) {
+            if (M[i][j] == 1 && visited[j] == 0) {
+                visited[j] = 1;
+                dfs(M, visited, j);
+            }
+        }
+    }
+}
