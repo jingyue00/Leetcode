84a45a23b5c3b89fd3550241ab4b848a536d28f9
jingyue00 <yuejing3963@gmail.com>
2018-07-02 23:17:10 -0400

    Lowest Common Ancestor of a Binary Search Tree

---
+++ src/LowestCommonAncestorofaBinarySearchTree.java
@@ -0,0 +1,18 @@
+/**
+ * Solution: According to the definition of BST, we can compare value of current node
+ *      and value of p and q to decide continue loop the left or right node.
+ * Time Complexity: O(n)
+ * Space Complexity: O(n)
+ */
+public class LowestCommonAncestorofaBinarySearchTree {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return root;
+        if (root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        } else {
+            return root;
+        }
+    }
+}
