63993d1f95e6501ace55e35f8841b100b24e515a
jingyue00 <yuejing3963@gmail.com>
2018-07-03 23:25:10 -0400

    Number of Islands

---
+++ src/NumberofIslands.java
@@ -0,0 +1,28 @@
+/**
+ * Solution: use dfs to check left right up down, if character is 1
+ * Time Complexity: O(m * n)
+ * Space Complexity: O(m * n)
+ */
+public class NumberofIslands {
+    public int numIslands(char[][] grid) {
+        int res = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+
+    public void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0') return;
+        grid[i][j] = '0';
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j - 1);
+    }
+}
