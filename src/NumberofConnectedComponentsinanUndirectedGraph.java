a023137efb9d316ee221e753b1fb149a9fd37c8a
jingyue00 <yuejing3963@gmail.com>
2018-07-06 22:25:30 -0400

    Number of Connected Components in an Undirected Graph

---
+++ src/NumberofConnectedComponentsinanUndirectedGraph.java
@@ -0,0 +1,32 @@
+/**
+ * Solution: Use int[] roots = new int[n] record corresponding points.
+ *      each time find the root
+ * Time Complexity: O(edges * nodes)
+ * Space Complexity: O(n)
+ */
+public class NumberofConnectedComponentsinanUndirectedGraph {
+    public int countComponents(int n, int[][] edges) {
+        int res = n;
+        int[] roots = new int[n];
+        for (int i = 0; i < n; i++) {
+            roots[i] = -1;
+        }
+        for (int[] pair : edges) {
+            int x = find(roots, pair[0]);
+            int y = find(roots, pair[1]);
+            if (x != y) {
+                roots[x] = y;
+                res --;
+            }
+        }
+        return res;
+    }
+
+    public int find(int[] roots, int i) {
+        while (roots[i] != -1) {
+            i = roots[i];
+        }
+        return i;
+    }
+
+}
